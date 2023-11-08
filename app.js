78e35d69d352bba619b7bb631b7febaa93a10b4a
33lucasant <lucassantgomez@gmail.com>
2023-11-07 21:46:11 -0300

    Modificando package.json y app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen(3000, () => console.log('Servidor funcionando en puerto 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Servidor funcionando en puerto ${PORT}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
